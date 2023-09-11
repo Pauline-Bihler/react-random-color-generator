f255864aafb75ba27fef19cfce7e626362041b18
Pauline Bihler <pauline.erika5@gmail.com>
2023-09-11 18:15:48 +0200

    Add code for the project

--- src/App.js
+++ src/App.js
@@ -1,3 +1,73 @@
+// Import libraries needed
+import randomColor from 'randomcolor';
+import { useState } from 'react';
+
 export default function App() {
-  return <div></div>;
+  const [color, setColor] = useState('#ffb6c1'); // Initial color
+  return (
+    <>
+      <br />
+      <br />
+      <h1
+        style={{
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: '#f0f8ff',
+          width: '360px',
+          height: '40px',
+          margin: '0 auto',
+          whiteSpace: 'nowrap',
+        }}
+      >
+        Random Color Generator
+      </h1>
+      <br />
+      <p
+        style={{
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: '#e6e6fa',
+          width: '320px',
+          height: '20px',
+          margin: '0 auto',
+          whiteSpace: 'nowrap',
+        }}
+      >
+        Generate a random color with a click of a button!
+      </p>
+      <br />
+      <br />
+      <div
+        style={{
+          backgroundColor: color,
+          width: '300px',
+          height: '300px',
+          margin: '0 auto',
+          textAlign: 'center',
+          lineHeight: '280px',
+        }}
+      >
+        Generated Color: {color}
+      </div>
+      <br />
+      <br />
+      <button
+        style={{
+          display: 'block',
+          margin: '0 auto',
+          width: '160px',
+          height: '40px',
+          textAlign: 'center',
+          lineHeight: '40px',
+          backgroundColor: '#f0f8ff',
+        }}
+        onClick={() => {
+          const newColor = randomColor();
+          setColor(newColor);
+        }}
+      >
+        Generate
+      </button>
+    </>
+  );
 }
